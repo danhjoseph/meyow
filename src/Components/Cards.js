70fda28f19825caebf38941318f79d8dd6511393
Dan Joseph <54128367+danhjoseph@users.noreply.github.com>
2022-06-09 12:45:11 -0400

    database init

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
-import "./Cards.css"
+import "./Cards.css";
 
 function Cards() {
   const [pets, setPets] = useState([]);
 
   return (
     <div>
-      {pets.map((pet) => (
-        <TinderCard
-          className="swipe"
-          key={pet.name}
-          preventSwipe={["up", "down"]}
-        >
-          <div style={{ backgroundImage: `url(${pet.img})` }} className="card">
-            <h3>{pet.name}</h3>
-          </div>
-        </TinderCard>
-      ))}
+      <div className="container">
+        {pets.map((pet) => (
+          <TinderCard
+            className="swipe"
+            key={pet.name}
+            preventSwipe={["up", "down"]}
+          >
+            <div
+              style={{ backgroundImage: `url(${pet.img})` }}
+              className="card"
+            >
+              <h3>{pet.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 }
