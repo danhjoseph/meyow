819729b9f068cbdd30a8e084bf5c8f9c85e931a2
danhjoseph <54128367+danhjoseph@users.noreply.github.com>
2022-06-10 15:40:28 -0400

    Update Firebase.js

--- src/Database/Firebase.js
+++ src/Database/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8rGUGZsyNd_5L_kUlnNNgbRMS-B3xFew",
-  authDomain: "meyow-7f1bb.firebaseapp.com",
-  projectId: "meyow-7f1bb",
-  storageBucket: "meyow-7f1bb.appspot.com",
-  messagingSenderId: "471028695555",
-  appId: "1:471028695555:web:c278c2e892d49dc6fa0436",
-  measurementId: "G-MKDH3HW8CZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
